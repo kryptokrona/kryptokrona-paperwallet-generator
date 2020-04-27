0484c7279e38b230c510213a881d3190a036c13a
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2020-04-27 09:54:21 +0200

    color code jdenticons until identicons is implemented

--- assets/js/jdenticon-settings.js
+++ assets/js/jdenticon-settings.js
@@ -3,13 +3,14 @@
 // Please see the included LICENSE file for more information.
 
 window.jdenticon_config = {
-  lightness: {
-    color: [0.41, 0.81],
-    grayscale: [0.41, 0.80]
-  },
-  saturation: {
-    color: 0.80,
-    grayscale: 0.41
-  },
-  backColor: "rgb(1, 38, 63)"
+  hues: [188],
+    lightness: {
+        color: [0.35, 0.83],
+        grayscale: [0.64, 0.64]
+    },
+    saturation: {
+        color: 0.81,
+        grayscale: 0.89
+    },
+    backColor: "#6cdeee00"
 };
